2768724dfa8e7ac163af9bd09973d4aef3f3b7f4
Eric <87290130+MTFPLEX@users.noreply.github.com>
2025-01-08 22:12:52 -0800

    Update index.php
    Removed phantom references

--- index.php
+++ index.php
@@ -1,13 +1,10 @@
-<?php 
-    include("connection.php");
-    include("login.php")
-    ?>
-    
+<?php
+    include("connection.php")
+?>
 <html>
     <head>
         <title>Login</title>
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
-        <link rel="stylesheet" type="text/css" href="style.css">
     </head>
     <body>
         
